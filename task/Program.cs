efc8dbc890ce416d01ef20dd55fdebfd3c702dcb
klimiwe <klimiwe@mail.ru>
2022-09-15 10:30:35 +0400

    Task

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+Примеры:
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+[“Russia”, “Denmark”, “Kazan”] → []
+
+*/
+string [] array = {"Russia", "Denmark", "Kazan"};
+PrintArray (array);
+
+string [] resultArray = ChangeArray (array);
+PrintArray (resultArray);
+
+void PrintArray(string [] array)
+{
+    Console.Write("[");
+    for(int i=0; i<array.Length;i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+   Console.WriteLine("]");
+}
+
+
+ string [] ChangeArray (string [] array)   
+ {
+    int n=0;
+for (int i =0; i<array.Length; i++)
+{
+    if (array[i].Length<=3) {n++;}
+}
+string [] resultArray = new string [n]; 
+int k=0;
+for (int j =0; j<array.Length; j++)
+{
+    if (array[j].Length<=3) {resultArray[k]= array[j]; k++;}
+}
+return resultArray;
+ }
